66c04dc9173a2a10cbd14cd661ba3bf5f6be8799
Lucas Cruz <lucascruz.joao@alu.ufc.br>
2021-08-11 02:19:29 -0300

    feat nav config add files

---
+++ app/src/main/java/com/example/drink_more_water/ui/ConfigTimerFragment.kt
@@ -0,0 +1,30 @@
+package com.example.drink_more_water.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.drink_more_water.R
+
+class ConfigTimerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_timer_config, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.buttonBack).setOnClickListener {
+            findNavController().navigate(R.id.action_ConfigTimerFragment_to_WaterTimerFragment)
+        }
+    }
+
+}
